e8dc8ef9ad38e61ec0bc49ae443307f9d3cd1130
Florian VINCENT <florian.vincent@inria.fr>
2024-12-06 09:55:54 +0100

    [FEAT] Added batched oracles for func in project 1

--- projects/Group_1/functions.py
+++ projects/Group_1/functions.py
@@ -1,6 +1,8 @@
+from typing import List, Union
 import numpy as np
 
 from baseloss import LossFunction
+
 
 class Function1(LossFunction):
     def __init__(self, A: np.ndarray, b: np.ndarray):
@@ -14,8 +16,9 @@ class Function1(LossFunction):
     def grad_oracle(self, theta: np.ndarray) -> np.ndarray:
         return self.A @ theta + self.b
 
-    def hessian_oracle(self, theta: np.ndarray) -> np.ndarray:
+    def hessian_oracle(self, _theta: np.ndarray) -> np.ndarray:
         return self.A
+
 
 class Function2(LossFunction):
     def __init__(self, X: np.ndarray, y: np.ndarray):
@@ -27,7 +30,7 @@ class Function2(LossFunction):
     def value_oracle(self, theta: np.ndarray) -> float:
         pred = self.X @ theta
         error = pred - self.y
-        return .5 * error.T @ error
+        return .5 * np.dot(error, error)
 
     def grad_oracle(self, theta: np.ndarray) -> np.ndarray:
         assert self.X.shape[1] == theta.shape[0]
@@ -35,3 +38,26 @@ class Function2(LossFunction):
         random_point = self.X[random_index, :]
         random_label = self.y[random_index]
         return random_point * (random_point @ theta - random_label)
+
+    def batched_value_oracle(
+        self,
+        indexing: Union[int, List[int], np.ndarray],
+        theta: np.ndarray
+    ):
+        assert np.max(indexing) < self.X.shape[0]
+        assert np.min(indexing) >= 0
+        pred = self.X[indexing, :] @ theta
+        error = pred - self.y[indexing]
+        return .5 * np.dot(error, error)
+
+    def batched_grad_oracle(
+        self,
+        indexing: Union[int, List[int], np.ndarray],
+        theta: np.ndarray
+    ):
+        assert np.max(indexing) < self.X.shape[0]
+        assert np.min(indexing) >= 0
+        selected_points = self.X[indexing, :]
+        pred = selected_points @ theta
+        error = pred - self.y[indexing]
+        return selected_points * error
